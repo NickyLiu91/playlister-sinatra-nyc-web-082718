38a1eb3420380cec79e37b362b25a1f7c9b04d02
Chris Bojemski <chris.bojemski@gmail.com>
2018-09-20 16:28:28 -0400

    Stuck again.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,7 +1,31 @@
+require_relative '../models/concerns/slugifiable'
+
 class SongsController < ApplicationController
+
   get '/songs' do
     @songs = Song.all
     erb :'songs/index'
+  end
+
+  post '/songs' do
+    puts params
+    artist = Artist.find_by(name: params['artist'])
+    if artist == nil
+      artist = Artist.create(name: params['artist'])
+    end
+    genre_names = params.select do |key, value|
+      Genre.all.include?(key)
+    end.keys
+    genres = genre_names.map do |genre|
+      Genre.find_by(name: genre)
+    end
+    song = Song.create(name: params['Name'], artist: artist, genres: genres)
+    binding.pry
+    redirect :"songs/#{song.slug}"
+  end
+
+  get '/songs/new' do
+    erb :'songs/new'
   end
 
   get '/songs/:name' do
@@ -12,4 +36,6 @@ class SongsController < ApplicationController
     @genres = @song.genres
     erb :'songs/show'
   end
+
+  include Slug
 end
